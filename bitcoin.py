88e98315e19b158feef25faa2bde95d1ff78534a
joelmatheweby <joeljothameby@gmail.com>
2026-08-05 20:25:35 +0530

    Added  Bitcoin API

--- bitcoin.py
+++ bitcoin.py
@@ -1,40 +1,29 @@
 import requests
-
-from config import ALPHA_VANTAGE_API_KEY
+from config import TWELVEDATA_API_KEY
 
 
 def get_bitcoin_price():
 
-    url = "https://www.alphavantage.co/query"
+    url = "https://api.twelvedata.com/price"
 
     params = {
-        "function": "CURRENCY_EXCHANGE_RATE",
-        "from_currency": "BTC",
-        "to_currency": "USD",
-        "apikey": ALPHA_VANTAGE_API_KEY
+        "symbol": "BTC/USD",
+        "apikey": TWELVEDATA_API_KEY
     }
 
     try:
-
         response = requests.get(url, params=params, timeout=10)
-
         response.raise_for_status()
 
         data = response.json()
 
-        if "Realtime Currency Exchange Rate" not in data:
+        if "price" not in data:
             return f"API Error: {data}"
 
-        exchange = data["Realtime Currency Exchange Rate"]
+        return f"""Current Bitcoin Price
 
-        price = exchange["5. Exchange Rate"]
-
-        return f"""
-Current Bitcoin Price
-
-USD : ${float(price):,.2f}
+USD : ${float(data['price']):,.2f}
 """
 
     except Exception as e:
-
         return f"Bitcoin API Error: {e}"
